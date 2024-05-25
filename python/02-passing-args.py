4f37b46de14c3711263c62056f72fd81c0a10d9d
amirshariati <seyed.amir.shariati@gmail.com>
2024-05-26 01:52:28 +0330

    add Query

--- python/02-passing-args.py
+++ python/02-passing-args.py
@@ -1,3 +1,14 @@
 import graphene
 
 
+class Query(graphene.ObjectType):
+    name = graphene.String(name=graphene.String(default_value='World', description='set name, default is world'), description='return Hello Name')
+    is_admin = graphene.Boolean()
+
+    def resolve_name(self, info, name):
+        return f'Hello {name}'
+
+    def resolve_is_admin(self, info):
+        return True
+
+
