8efa907f1bfe156d16d074359ecece99de095cc1
amirshariati <seyed.amir.shariati@gmail.com>
2024-05-26 01:29:06 +0330

    add Query

--- python/01-query.py
+++ python/01-query.py
@@ -2,3 +2,10 @@ import graphene
 import json
 
 
+class Query(graphene.ObjectType):
+    name = graphene.String(description='return Hello world')
+
+    def resolve_name(self, info):
+        return 'Hello world'
+
+
