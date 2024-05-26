f726fe8b7136e3eae6d5f445ee01852b782b1a89
amirshariati <seyed.amir.shariati@gmail.com>
2024-05-26 16:51:29 +0330

    add Query class

--- python/05-context.py
+++ python/05-context.py
@@ -1,3 +1,10 @@
 import graphene
 
 
+class Query(graphene.ObjectType):
+    name = graphene.String(name=graphene.String(default_value='World'))
+
+    def resolve_name(self, info, name):
+        return f'Hello {info.context.get("username")}'
+
+
