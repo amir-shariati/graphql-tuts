a83ead264e9469f65fc541c54dea866118e291bd
amirshariati <seyed.amir.shariati@gmail.com>
2024-05-26 16:24:45 +0330

    add User class

--- python/04-inputObjectTypes.py
+++ python/04-inputObjectTypes.py
@@ -3,3 +3,9 @@ import uuid
 from datetime import datetime
 
 
+class User(graphene.ObjectType):
+    id = graphene.String(default_value=uuid.uuid4())
+    username = graphene.String()
+    created_at = graphene.DateTime(default_value=datetime.now())
+
+
