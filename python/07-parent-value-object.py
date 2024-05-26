6074ac61273840440e8633f1e9617c6ff18e95fb
amirshariati <seyed.amir.shariati@gmail.com>
2024-05-27 01:01:44 +0330

    add User class

--- python/07-parent-value-object.py
+++ python/07-parent-value-object.py
@@ -3,3 +3,14 @@ import uuid
 from datetime import datetime
 
 
+class User(graphene.ObjectType):
+    id = graphene.String(default_value=uuid.uuid4())
+    username = graphene.String()
+    created_at = graphene.DateTime(default_value=datetime.now())
+
+    @staticmethod
+    def resolve_username(root, info):
+        print(f'root is {root}')
+        return root.get('name')
+
+
