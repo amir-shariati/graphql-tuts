9e23dad7fccde8bfb0adcc9ed357b9a5580b0834
amirshariati <seyed.amir.shariati@gmail.com>
2024-05-30 00:19:33 +0330

    add UserType

--- django/accounts/schema.py
+++ django/accounts/schema.py
@@ -3,3 +3,8 @@ from graphene_django.types import DjangoObjectType, ObjectType
 from django.contrib.auth.models import User
 
 
+class UserType(DjangoObjectType):
+    class Meta:
+        model = User
+
+
